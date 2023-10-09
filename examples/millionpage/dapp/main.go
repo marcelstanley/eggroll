37518c72de614c730e04e4fa366e2bbcc9862ca4
Marcel Moura <marcelstanley@users.noreply.github.com>
2023-10-09 15:33:49 -0300

    fixup! Add million CTSI homepage example

---
+++ examples/millionpage/dapp/main.go
@@ -0,0 +1,60 @@
+// Copyright (c) Gabriel de Quadros Ligneul
+// Copyright (c) Marcel Moura
+// SPDX-License-Identifier: MIT (see LICENSE)
+
+// Package that defines the contract logic
+package main
+
+import (
+	"fmt"
+	"image"
+	"million"
+
+	"github.com/gligneul/eggroll"
+	"github.com/kelindar/bitmap"
+)
+
+// DApp contract
+type MillionContract struct {
+	eggroll.DefaultContract
+	Pixels bitmap.Bitmap
+}
+
+func (c MillionContract) Clear() {
+	c.Pixels.Clear()
+}
+
+func (c MillionContract) Paint(p image.Point) error {
+	c.Pixels.Set(uint32(p.X + p.Y*1000))
+	//fmt.Errorf("invalid coordinates")
+	return nil
+}
+
+func (c *MillionContract) Decoders() []eggroll.Decoder {
+	return []eggroll.Decoder{
+		eggroll.NewGenericDecoder[million.Init](),
+		eggroll.NewGenericDecoder[million.Paint](),
+	}
+}
+
+func (c *MillionContract) Advance(env *eggroll.Env, input any) ([]byte, error) {
+	switch input := input.(type) {
+	case *million.Init:
+		env.Logf("received input Init. State= %v", c)
+		c.Clear()
+
+	case *million.Paint:
+		// TODO Check for valid coordinates. Only free pixels are available
+		env.Logf("received input Paint with '%v'\n", input)
+		if err := c.Paint(input.Point); err != nil {
+			return nil, err
+		}
+	default:
+		return nil, fmt.Errorf("invalid input")
+	}
+	return c.Pixels.ToBytes(), nil // Returning the bitmap because we can
+}
+
+func main() {
+	eggroll.Roll(&MillionContract{})
+}
--- examples/millionpage/million.go
+++ examples/millionpage/million.go
@@ -1,4 +1,3 @@
-// Copyright (c) Gabriel de Quadros Ligneul
 // Copyright (c) Marcel Moura
 // SPDX-License-Identifier: MIT (see LICENSE)
 
@@ -6,60 +5,16 @@
 package million
 
 import (
-	"fmt"
 	"image"
 	"image/color"
-
-	"github.com/gligneul/eggroll"
-	"github.com/kelindar/bitmap"
 )
 
-type (
-	// Input type Init forces the initialization of the DApp state
-	Init struct {
-	}
-
-	// Input type Paint specifies a pixel to be painted
-	Paint struct {
-		Point image.Point
-		Color color.RGBA
-	}
-)
-
-// DApp contract
-type Contract struct {
-	Pixels bitmap.Bitmap
+// Input type Init forces the initialization of the DApp state
+type Init struct {
 }
 
-func (c Contract) Clear() {
-	c.Pixels.Clear()
-}
-
-func (c Contract) Paint(p image.Point) error {
-	c.Pixels.Set(uint32(p.X + p.Y*1000))
-	return nil
-}
-
-func (c *Contract) Decoders() []eggroll.Decoder {
-	return []eggroll.Decoder{
-		eggroll.NewGenericDecoder[Init](),
-		eggroll.NewGenericDecoder[Paint](),
-	}
-}
-
-func (c *Contract) Advance(env *eggroll.Env, input any) error {
-	switch input := input.(type) {
-	case *Init:
-		c.Clear()
-		env.Logf("received input Init. State= %v", c)
-	case *Paint:
-		// TODO Check for valid coordinates. Only free pixels are available
-		env.Logf("received input Paint with '%v'\n", input)
-		if c.Paint(input.Point) != nil {
-			return fmt.Errorf("invalid coordinates")
-		}
-	default:
-		return fmt.Errorf("invalid input")
-	}
-	return nil
+// Input type Paint specifies a pixel to be painted
+type Paint struct {
+	Point image.Point
+	Color color.RGBA
 }
